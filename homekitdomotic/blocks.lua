eb67d75492bb1cd30d82e448307a6b64a2dc02f8
Bernard <bernard.pinochet@yahoo.fr>
2021-02-06 00:11:42 +0100

    Fin du développement du jour => La dalle magique allume la lampe hue du bureau!

--- homekitdomotic/blocks.lua
+++ homekitdomotic/blocks.lua
@@ -15,5 +15,50 @@ minetest.register_node("homekitdomotic:dalle", {
 			--{-0.5, 0.0, 0.0, 0.5, 0.5, 0.5},
 			--{-0.5, 0.0, -0.5, 0.0, 0.5, 0.0},
 		
-	}
+	},
+	on_blast=function(pos,intensity)
+		domotic.hue.off("bureau Paul")
+		domotic.hue.off("Lampe Paul")
+	end,
+	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+		domotic.hue.on("bureau Paul")
+		domotic.hue.on("Lampe Paul")
+	end,
+	hue_name="bureau Paul"
 })
+minetest.register_node("homekitdomotic:tour", {
+	description = "Tour",
+	is_ground_content = false,
+	tile_images = {"hkd_blocks_wood.png", "hkd_blocks_wood.png", "hkd_blocks_tour.png",
+		"hkd_blocks_wood.png", "hkd_blocks_wood.png", "hkd_blocks_wood.png"},
+})
+
+-- Toutes les 100 ms on detecte si un joueur se trouve sur une dalle magique
+local state=false
+local elapsed=0
+minetest.register_globalstep(function(dtime)
+
+        elapsed=elapsed+dtime
+        if (elapsed>0.1) then
+		players=minetest.get_connected_players()
+		new_state=false
+		for _,p in pairs(players) do
+			pos=p:get_pos()
+			node = minetest.get_node(pos)
+			if (node.name=="homekitdomotic:dalle") then
+				new_state=true
+			end
+		end
+
+		if (new_state~=state) then
+			if (new_state==true) then
+				domotic.hue.on("paul Bureau")
+			else
+				domotic.hue.off("paul Bureau")
+			end
+
+			state=new_state
+		end
+	end
+end)
+
--- homekitdomotic/init.lua
+++ homekitdomotic/init.lua
@@ -4,6 +4,8 @@
 -- en utilisant le MOD domotic
 --
 
+dofile(minetest.get_modpath("homekitdomotic") .. "/blocks.lua")
+
 domotic.chat.send('Start of minetest HOME KIT DOMOTIC MOD')
 
 
@@ -28,5 +30,39 @@ local function cbCardInserted(idCard)
 end
 
 domotic.badge.register_on_card_inserted(cbCardInserted)
-	
+
+
+local function get_formspec(name)
+    -- TODO: display whether the last guess was higher or lower
+    local text = "I'm thinking of a number... Make a guess!"
+
+    local formspec = {
+        "formspec_version[4]",
+        "size[6,3.476]",
+        "label[0.375,0.5;", minetest.formspec_escape(text), "]",
+        "field[0.375,1.25;5.25,0.8;number;Number;]",
+        "button[1.5,2.3;3,0.8;guess;Guess]"
+    }
+
+    -- table.concat is faster than string concatenation - `..`
+    return table.concat(formspec, "")
+end
+
+local function show_to(name)
+    minetest.show_formspec(name, "guessing:game", get_formspec(name))
+end
+
+minetest.register_chatcommand("game", {
+    func = function(name)
+        show_to(name)
+    end,
+})
+
+
+minetest.register_chatcommand("tv", {
+    func = function(name)
+	name="bureau Paul" 
+       	domotic.hue.off(name)
+    end,
+})
 
